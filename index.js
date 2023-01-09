993e4a500cde3b5375752ffc5d3062638c0904a6
Developerabdullah <97086882+DeveleplorAbdullah@users.noreply.github.com>
2023-01-10 07:25:22 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const firstFlight = "1. 1AM German to New York";
+console.log(firstFlight);
+console.log(typeof firstFlight);
+const secondFlight = "2. 1.40AM German to Brazil";
+console.log(secondFlight);
+console.log(typeof secondFlight)
+const thirdFlight = "3. 2.50AM German to Russia";
+console.log(thirdFlight);
+const fourthFlight = "4. German to India";
+console.log(fourthFlight);
+const fifthFlight = "5. German to Center of Africa";
+console.log(fifthFlight);
+const sixthFlight = "6. German to Japan";
+console.log(sixthFlight);
+const seventhFlight = "7. German to Argentina";
+console.log(seventhFlight);
